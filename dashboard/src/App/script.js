b84d504c3c5e79c0545b0f72de50cf0ef63966a9
x1nyuan <yao.ntno@gmail.com>
2018-03-10 20:26:44 +0900

    Add components

--- dashboard/src/App/script.js
+++ dashboard/src/App/script.js
@@ -1,8 +1,10 @@
 import PageHeader from '@/components/PageHeader'
+import PageSideBar from '@/components/PageSideBar'
 
 export default {
   name: 'app',
   components: {
     PageHeader,
+    PageSideBar ,
   }
 }
