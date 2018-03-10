b3c2ec5b26c21d9981c21dd31ec9cd782668d884
x1nyuan <yao.ntno@gmail.com>
2018-03-10 18:06:26 +0900

    add datetimepicker

--- dashboard/src/App/script.js
+++ dashboard/src/App/script.js
@@ -1,8 +1,10 @@
 import HelloWorld from '@/components/HelloWorld'
+import { Datetime } from 'vue-datetime';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Datetime,
   }
 }
--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'vue-datetime/dist/vue-datetime.css'
 
 Vue.config.productionTip = false
 
