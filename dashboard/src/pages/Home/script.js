4dadb96b6215080414e3c584008987fd126813d9
x1nyuan <yao.ntno@gmail.com>
2018-03-17 19:21:14 +0900

    add d3 and lodash

--- dashboard/src/pages/Home/script.js
+++ dashboard/src/pages/Home/script.js
@@ -1,7 +1,42 @@
 import PageContent  from "@/components/PageContent";
+import LineChart  from "@/components/LineChart";
 export default {
   components: {
     PageContent,
+    LineChart,
+  },
+  data: function () {
+    return{
+      plotData1:[
+        {
+          x: 1,
+          y: 1,
+        },
+        {
+          x: 2,
+          y: -1,
+        },
+        {
+          x: 3,
+          y: 2,
+        },
+      ],
+
+      plotData2:[
+        {
+          x: 1,
+          y: -1,
+        },
+        {
+          x: 2,
+          y: 1,
+        },
+        {
+          x: 3,
+          y: 2,
+        },
+      ]
+    }
   },
   name: 'Home',
 }
