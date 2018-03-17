f12ba65fe77a95cd11090d5595c85164fc758577
x1nyuan <yao.ntno@gmail.com>
2018-03-17 19:21:19 +0900

    todays commit

--- dashboard/src/components/PageHeader/script.js
+++ dashboard/src/components/PageHeader/script.js
@@ -1,3 +1,8 @@
 export default {
   name: 'PageHeader',
+  data:function () {
+    return {
+      showNav: false
+    };
+  }
 }
--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -15,6 +15,11 @@ export default new Router({
       path: '/about',
       name: 'About',
       component: pages.About
-    }
+    },
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: pages.Blog
+    },
   ]
 })
